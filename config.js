8bc1c1f2013752987ef8c8993a4eec7124e4ad31
credencehighschool <physicaleducation@credencehighschool.com>
2026-07-08 17:47:50 +0400

    Add files via upload

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ const SITE_CONFIG = {
   entryDeadline: "Friday, June 12th 2026",
 
   // IMPORTANT: paste your deployed Google Apps Script Web App URL here (see Code.gs)
-  apiUrl: "https://script.google.com/macros/s/AKfycbzzOQikg9rPWT4IJkFPqiReQkanuEG3t78Cr6mLeYAuCsx4EB1u_drf6rn-RvD29aaV/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbzR0-gaRmeL9iJs9p3XbG06atHiqFN_xHHbmVY43Uk_pNnLLIv222Sg3L-gkPpqONxAGg/exec",
   registrationUrl: "https://script.google.com/macros/s/AKfycbwqyephi8z4lTx7Mr8FFcMBNz51mfXaVvYl95WeR1pwyDpkr2rOtvI6-WifODrU_kX5cA/exec",
 
   // Turn each section on once it's ready — controls the Home page tiles
